454d33b3505696ceb542749bb02114cacf83c30f
Koen van Gasteren <koen.vangasteren@tangelo.nl>
2025-10-23 09:17:35 +0200

    minimal schema public sessions and hands

---
+++ drizzle/mirgrations/0000_third_expediter.sql
@@ -0,0 +1,26 @@
+CREATE TABLE "hands" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"session_id" uuid NOT NULL,
+	"index" integer NOT NULL,
+	"base_scores" jsonb NOT NULL,
+	"doubles" jsonb NOT NULL,
+	"winner_name" varchar(255) NOT NULL,
+	"east_name" varchar(255) NOT NULL,
+	"final_scores" jsonb NOT NULL,
+	"deltas" jsonb NOT NULL,
+	"hand_note" varchar(2000),
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "sessions" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"players" jsonb NOT NULL,
+	"play_date" date NOT NULL,
+	"picture_url" varchar(2048),
+	"session_note" varchar(4000),
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "hands" ADD CONSTRAINT "hands_session_id_sessions_id_fk" FOREIGN KEY ("session_id") REFERENCES "public"."sessions"("id") ON DELETE cascade ON UPDATE no action;
